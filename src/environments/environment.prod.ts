2231e6b256f3c0c5d6e19dc4c1207b88d67c7c6f
Kevin Unger <kevinunger98@gmail.com>
2023-04-18 19:27:11 +0200

    init2

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'gpsportplaner',
+    appId: '1:555972866190:web:376a8a8dadf8fc069a99df',
+    storageBucket: 'gpsportplaner.appspot.com',
+    apiKey: 'AIzaSyDzhl7j9xSVlLb1zolOCEb80d6gwwHvzdE',
+    authDomain: 'gpsportplaner.firebaseapp.com',
+    messagingSenderId: '555972866190',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'gpsportplaner',
+    appId: '1:555972866190:web:376a8a8dadf8fc069a99df',
+    storageBucket: 'gpsportplaner.appspot.com',
+    apiKey: 'AIzaSyDzhl7j9xSVlLb1zolOCEb80d6gwwHvzdE',
+    authDomain: 'gpsportplaner.firebaseapp.com',
+    messagingSenderId: '555972866190',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
