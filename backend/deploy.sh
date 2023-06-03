2b68b29debf9b66c3374290bb45142e0faaae3d4
Kevin Unger <kevinunger98@gmail.com>
2023-06-03 20:35:17 +0200

    ch deploy

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -4,8 +4,10 @@ pm2 kill
 echo "Jump to app folder"
 cd ~/actions-runner/_work/GPSportPlaner/GPSportPlaner/backend
 
-echo "Sourcing Env Vars"
-bash /home/gps/envs/envs.sh
+export NODE_ENV=production
+
+echo "Copying .env.production from /home/gps/envs/.env.production"
+cp /home/gps/envs/.env.production .env.production
 
 echo "Run new PM2 action"
 pm2 start "npm run start" --update-env
