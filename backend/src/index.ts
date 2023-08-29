1f07e1aa132ed6c410695ac7adc412b212efc493
Kevin Unger <kevinunger98@gmail.com>
2023-08-29 12:36:46 +0200

    rm secret log

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => {
     console.log(`using env: ${env}`);
     console.log(`[server]: Server is running at http://localhost:${port}`);
-    console.log(process.env.JWT_SECRET);
+    // console.log(process.env.JWT_SECRET);
   });
 }
 
